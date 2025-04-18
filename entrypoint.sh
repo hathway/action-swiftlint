f1a059c560385290a6a447150ba3c6775b915865
Nick Beadman <nbeadman@users.noreply.github.com>
2025-04-18 15:18:52 -0700

    SwiftLint 0.58.2 (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,4 +41,5 @@ then
 	fi
 fi
 
+echo "Using swiftlint version: `swiftlint --version`"
 set -o pipefail && swiftlint "$@" -- $changedFiles | stripPWD | convertToGitHubActionsLoggingCommands | parseOutputsParameters
